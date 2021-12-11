8ba0bcccbdb078e714097eba162c2bdfedaccd95
kiarash <kiarashmohsenipoor99@gmail.com>
2021-12-12 02:42:03 +0330

    change Ui.js, Ui.css and Au.js

---
+++ src/components/Ui.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Au from '../hoc/Au';
+import Header from './Header/Header';
+import Board from "./Board/Board";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Modal from "./Modal/Modal";
+import Refresh from '@material-ui/icons/Refresh';
+
+
+class Ui extends Component {
+    constructor(props) {
+        super(props);
+        this.winner = this.winner.bind(this);
+        this.state = {
+            board: <Board size={props.size} won={this.winner} key="1" />,
+            open: false,
+            score: false,
+            text: ''
+        }
+    }
+
+    winner = text => {
+        this.setState({
+            open: true,
+            text: text
+        })
+    };
+
+    resetGame = () => {
+        const value = Math.random().toString(36).substring(7);
+        this.setState({
+            board: <Board size={this.props.size} won={this.winner} key={value}/>
+        })
+    };
+
+    showScore = () => {
+        this.setState({
+            score: true
+        })
+    };
+    handleClose = () => {
+        this.setState({open: false});
+        this.resetGame();
+    };
+
+    closeScore = () => {
+        this.setState({score: false});
+    };
+
+    clearScore = () => {
+        localStorage.setItem('DRAWS',0);
+        localStorage.setItem('WONS',0);
+        localStorage.setItem('LOSS',0);
+        this.setState({score: false});
+        this.setState({score: true});
+    };
+
+
+    render() {
+        return (
+            <Au>
+                <Header />
+                {this.state.board}
+                <Grid container justify='center' spacing={24} className="Grid">
+                    <Grid item>
+                        <Button
+                            onClick={this.resetGame}
+                            variant="raised"
+                            color="default">
+                            <Refresh style={{ color: '#E91E63' }} />
+                            RESET
+                        </Button>
+                    </Grid>
+                </Grid>
+                <Modal
+                    text={this.state.text}
+                    open={this.state.open}
+                    onClose={this.handleClose} />
+            </Au>
+        )
+    }
+
+}
+
+Ui.propTypes = {
+    size: PropTypes.number
+};
+
+export default Ui;
