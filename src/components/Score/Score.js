7359ed0733304ce48446194f8669b16842d87205
kiarash <kiarashmohsenipoor99@gmail.com>
2021-12-12 03:33:55 +0330

    Score.js and Score.css has changed

---
+++ src/components/Score/Score.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Au from '../../../hoc/Au';
+import "./Score.css"
+
+
+const scorecard = (props) => {
+
+    const {open, onClose, clearScore} = props;
+
+    const action = [
+        <IconButton
+            key="delete"
+            aria-label="delete"
+            color="inherit"
+            onClick={clearScore}
+            >
+            <DeleteIcon style={{color:'#E91E63'}}/>
+        </IconButton>,
+        <IconButton
+            key="close"
+            aria-label="Close"
+            color="inherit"
+            onClick={onClose}
+            >
+            <CloseIcon style={{color:'#E91E63'}}/>
+        </IconButton>
+    ];
+
+    const draws = localStorage.getItem('DRAWS');
+    const wons = localStorage.getItem('WONS');
+    const loss = localStorage.getItem('LOSS');
+
+    const score = (
+        <Au>
+            <Divider className="Divider"/>
+            <Grid container spacing={16}>
+                <Grid item xs={4} >
+                    <Typography variant="body2" color="secondary" className="Scores">
+                        YourWins
+                    </Typography>
+                </Grid>
+                <Grid item xs={4} >
+                    <Typography variant="body2" color="secondary" className="Scores">
+                        Draws
+                    </Typography>
+                </Grid>
+                <Grid item xs={4} >
+                    <Typography variant="body2" color="secondary" className="Scores">
+                        OppWins
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Divider className="Divider"/>
+            <Grid container style={{marginTop:'5px'}}>
+                <Grid item xs={4}>
+                    <Typography variant="caption" color="secondary" className="Scores">
+                        {wons===null?0:wons}
+                    </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <Typography variant="caption" color="secondary" className="Scores">
+                        {draws===null?0:draws}
+                    </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <Typography variant="caption" color="secondary" className="Scores">
+                        {loss===null?0:loss}
+                    </Typography>
+                </Grid>
+            </Grid>
+
+        </Au>
+    );
+
+    return (
+        <Snackbar
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+            }}
+            open={open}
+            ContentProps={{
+                'aria-describedby': 'message-id',
+            }}
+            message={score}
+            action={action}
+        />
+    );
+
+};
+
+scorecard.propTypes = {
+    open: PropTypes.func,
+    onClose: PropTypes.func,
+    clearScore: PropTypes.func
+};
+
+
+export default scorecard;
